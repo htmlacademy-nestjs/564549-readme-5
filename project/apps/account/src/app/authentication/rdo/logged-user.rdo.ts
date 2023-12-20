bafed5b7fbccb13074afc760722b9372fbf24d46
Pavel Grigorev <grigorev-pv@sber-zvuk.com>
2023-12-20 21:14:03 +0300

    module2-task1(user): add user controller

---
+++ project/apps/account/src/app/authentication/rdo/logged-user.rdo.ts
@@ -0,0 +1,30 @@
+import { Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LoggedUserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '13'
+  })
+  @Expose()
+  public id: string;
+
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local'
+  })
+
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local'
+  })
+  @Expose()
+  public email: string;
+
+  @ApiProperty({
+    description: 'Access token',
+    example: 'user@user.local'
+  })
+  @Expose()
+  public accessToken: string;
+}
